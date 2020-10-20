94ca6fc3bab3443afa8f0120bb3b0aff2a22c9ee
kbaburao <kbrao981@gmail.com>
2020-10-20 14:21:01 +0530

    first commit

---
+++ PythonProgrammingLab/Lab-1.py
@@ -0,0 +1,6 @@
+''' 1.	Write a program that asks the user for a weight in kilograms and converts it to pounds. There are 2.2 pounds in a kilogram. '''
+
+
+kilogram = float(input("Pls - Enter a weight in Kilograms: "))
+pound=kilogram*2.20462
+print("after convert Kilograms to Pounds, The Ans is \t :\t{}\t".format(pound))
